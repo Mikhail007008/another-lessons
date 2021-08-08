ec791c8b14959251d53642f57a5ac91d02970560
Mikhail007008 <kasseta136@mail.ru>
2021-08-08 15:10:27 +0300

    fix(harder): fix lesson03

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 'use strict';
 
-const rus = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вск'];
-const en = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-
-const lang = prompt('Выберете язык', 'рус / en');
+let lang = prompt('Выберете язык', 'рус / en');
 
 if (lang === 'рус'){
 	console.log('Пн Вт Ср Чт Пт Сб Вск');
 } else if(lang === 'en'){
 	console.log('Mon Tue Wed Thu Fri Sat Sun');
 }
+
 
 switch (lang){
 	case 'рус': 
@@ -21,7 +18,9 @@ switch (lang){
 		break;
 }
 
-const result = lang === 'рус' ? console.log(rus) : console.log(en);
+let langAnsw = [['рус'], ['en']];
+langAnsw[0] = 'рус'; 
+let result = langAnsw[0] === lang ? console.log('Пн Вт Ср Чт Пт Сб Вск') : console.log('Mon Tue Wed Thu Fri Sat Sun');
 
 const namePerson = prompt('Введите имя', 'Артем / Максим / другое');
 let result1 = namePerson === 'Артем' ? alert('Директор') : 
